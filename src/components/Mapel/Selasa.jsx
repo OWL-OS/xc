31dbb7cf402bb81398d8a6f725315cdd7b6b7255
Rᴀʏʜᴀɴ⁷⁷ <109618146+SayuZX@users.noreply.github.com>
2024-08-21 09:07:33 +0700

    Update Selasa.jsx

--- src/components/Mapel/Selasa.jsx
+++ src/components/Mapel/Selasa.jsx
@@ -19,12 +19,17 @@ const Selasa = () => {
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
 					<div className="w-[50%] text-base  font-medium">PAI</div>
-					<div className="flex justify-center items-center text-sm">11.00-13.30</div>
+					<div className="flex justify-center items-center text-sm">11.00-11.45</div>
 				</div>
 
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
 					<div className="w-[50%] text-base  font-medium">ISTIRAHAT</div>
 					<div className="flex justify-center items-center text-sm">11.45-12.00</div>
+				</div>
+		
+		                <div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+					<div className="w-[50%] text-base  font-medium">PAI</div>
+					<div className="flex justify-center items-center text-sm">12.00-13.30</div>
 				</div>
         </>
     )
