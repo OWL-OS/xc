15d86dc5441b47b61d6fc57537e49fd8224b945f
Rᴀʏʜᴀɴ⁷⁷ <109618146+SayuZX@users.noreply.github.com>
2024-09-09 22:49:34 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './wiki.html'
+import '/wiki.html'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
